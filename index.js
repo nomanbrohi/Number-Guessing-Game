42267717c648836e21b6ca50f89e055d1d98c409
nomanbrohi <nomanbrohi9@gmail.com>
2024-03-27 13:33:02 +0500

    game updated with more functions

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
 #! /usr/bin/env node
-import * as readline from "readline";
-function user_input() {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    rl.question("Enter Your Number: ", (userInputs) => {
-        const usernumber = parseInt(userInputs);
-        const randomNumber = Math.floor(Math.random() * 100);
-        if (randomNumber === usernumber) {
-            console.log("Your Win." + " " + randomNumber + " is the correct number");
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blue("Welcome to Number Guessing Game"));
+let points = 0;
+let life = 3;
+while (true) {
+    const randomNumber = Math.floor(Math.random() * 6 + 1);
+    const answer = await inquirer.prompt([
+        {
+            message: chalk.yellow("Guess a number between 1 and 6"),
+            name: "number",
+        },
+    ]);
+    if (answer.number === "exit") {
+        console.log("you Exited the Game");
+        console.log("Thank You for Your Playing");
+        console.log("You Earn " + points + " Points. \nCome back again ");
+        break;
+    }
+    if (parseInt(answer.number) === randomNumber) {
+        points += 5;
+        console.log("You guessed the correct number. " +
+            randomNumber +
+            " You Won " +
+            points +
+            " Points.");
+        const wantToPlayAgain = await inquirer.prompt([
+            {
+                message: "Do you want to play again?",
+                name: "playAgain",
+                type: "list",
+                choices: ["Yes", "No"],
+            },
+        ]);
+        if (wantToPlayAgain.playAgain === "Yes") {
+            continue;
         }
         else {
-            console.log("Your Fail." + " " + randomNumber + " is the correct number");
+            break;
         }
-        rl.close();
-    });
+    }
+    else if (parseInt(answer.number) != randomNumber) {
+        life -= 1;
+        console.log("You loss! " + randomNumber + " is the correct number.");
+        if (life == 2 || life == 1) {
+            console.log("Try again ");
+            console.log("You have " + life + " life left");
+        }
+        else {
+            console.log("You have just Earned " + points + " Points.");
+            console.log("Game Over!!");
+            break;
+        }
+    }
+    else {
+        console.log("Invalid input");
+        console.log("Try again");
+        break;
+    }
 }
-user_input();
--- index.ts
+++ index.ts
@@ -1,27 +1,63 @@
 #! /usr/bin/env node
 
-import * as readline from "readline"
+import inquirer from "inquirer";
+import chalk from "chalk";
 
-function user_input():void{
-    const rl = readline.createInterface({
-        input:process.stdin,
-        output:process.stdout
-    })
+console.log(chalk.blue("Welcome to Number Guessing Game"));
+let points: number = 0;
+let life: number = 3;
+while (true) {
+  const randomNumber = Math.floor(Math.random() * 6 + 1);
+  const answer = await inquirer.prompt([
+    {
+      message: chalk.yellow("Guess a number between 1 and 6"),
+      name: "number",
+    },
+  ]);
 
-    rl.question("Enter Your Number: ", (userInputs:string) => {
-        const usernumber:number = parseInt(userInputs);
-        const randomNumber = Math.floor(Math.random() * 100)
+  if (answer.number === "exit") {
+    console.log("you Exited the Game")
+    console.log("Thank You for Your Playing")
+    console.log("You Earn " + points + " Points. \nCome back again ")
+    break;
+  }
 
-        if (randomNumber === usernumber){
-            console.log("Your Win." + " " + randomNumber + " is the correct number")
-        }
-        else{
-            console.log("Your Fail." + " " + randomNumber + " is the correct number")
-        }
-    rl.close()
-
-    })
-
-    }    
-
-user_input()
+  if (parseInt(answer.number) === randomNumber) {
+    points += 5;
+    console.log(
+      "You guessed the correct number. " +
+        randomNumber +
+        " You Won " +
+        points +
+        " Points."
+    );
+    const wantToPlayAgain = await inquirer.prompt([
+      {
+        message: "Do you want to play again?",
+        name: "playAgain",
+        type: "list",
+        choices: ["Yes", "No"],
+      },
+    ]);
+    if (wantToPlayAgain.playAgain === "Yes") {
+      continue;
+    } else {
+      break;
+    }
+  } else if (parseInt(answer.number) != randomNumber) {
+    life -= 1;
+    console.log("You loss! " + randomNumber + " is the correct number.");
+    if (life == 2 || life == 1) {
+      console.log("Try again ");
+      console.log("You have " + life + " life left");
+    } else {
+      console.log("You have just Earned " + points + " Points.")
+        console.log("Game Over!!")
+      break;
+    }
+  } else {
+    console.log("Invalid input");
+    console.log("Try again");
+    break;
+  }
+}
